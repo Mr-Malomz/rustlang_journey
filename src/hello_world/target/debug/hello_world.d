c9eb76bcce813e1ce806ee4a6b0a1477c1f400d4
Demola Malomo <malomo.alaba@yahoo.com>
2020-07-02 18:45:00 +0100

    first rustang program

---
+++ src/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\hp\Desktop\SAP\Web\ Dev\rust\rustlang_journey\src\hello_world\target\debug\hello_world.exe: C:\Users\hp\Desktop\SAP\Web\ Dev\rust\rustlang_journey\src\hello_world\src\main.rs
