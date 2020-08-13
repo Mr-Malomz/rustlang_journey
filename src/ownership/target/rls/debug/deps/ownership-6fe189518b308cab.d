96d42650bcbd1d6a5b84be4585f0f79fbaf22cd4
Demola Malomo <malomo.alaba@yahoo.com>
2020-08-13 13:36:41 +0100

    intro to ownership in rust

---
+++ src/ownership/target/rls/debug/deps/ownership-6fe189518b308cab.d
@@ -0,0 +1,5 @@
+c:\Users\hp\Desktop\SAP\Web Dev\rust\rustlang_journey\src\ownership\target\rls\debug\deps\ownership-6fe189518b308cab.rmeta: src\main.rs
+
+c:\Users\hp\Desktop\SAP\Web Dev\rust\rustlang_journey\src\ownership\target\rls\debug\deps\ownership-6fe189518b308cab.d: src\main.rs
+
+src\main.rs:
---
+++ src/ownership/target/rls/debug/deps/ownership-8b16677e7ef2d3f4.d
@@ -0,0 +1,5 @@
+c:\Users\hp\Desktop\SAP\Web Dev\rust\rustlang_journey\src\ownership\target\rls\debug\deps\ownership-8b16677e7ef2d3f4.rmeta: src\main.rs
+
+c:\Users\hp\Desktop\SAP\Web Dev\rust\rustlang_journey\src\ownership\target\rls\debug\deps\ownership-8b16677e7ef2d3f4.d: src\main.rs
+
+src\main.rs:
